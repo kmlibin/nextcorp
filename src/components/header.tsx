e69a09db89d6d384b29075233f8408e0ab6a878a
Kelli Libin <km.putnam2@gmail.com>
2023-12-27 09:38:54 -0700

    styled header

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,13 +1,18 @@
 import Link from "next/link";
 
-export default function Header () {
-    return (
-
-        <div>
-        <Link href="/">Home</Link>
-        <Link href="/performance">Performance</Link>
-        <Link href="/reliability">Reliability</Link>
-        <Link href="/scale">Scale</Link>
-      </div>
-    )
+export default function Header() {
+  return (
+    <div className="absolute z-10 w-full text-white">
+      <nav className="relative mx-auto flex flex-wrap items-center justify-between p-8">
+        <Link className="text-3xl font-bold" href="/">
+          Home
+        </Link>
+        <div className="space-x-4 text-xl">
+          <Link href="/performance">Performance</Link>
+          <Link href="/reliability">Reliability</Link>
+          <Link href="/scale">Scale</Link>
+        </div>
+      </nav>
+    </div>
+  );
 }
